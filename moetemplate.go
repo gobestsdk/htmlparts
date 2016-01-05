1c2b531be9fccf4d26282475e77fe312ce2bc5ca
timeloveboy <734991033@qq.com>
2016-01-05 21:05:01 +0800

    s

--- moetemplate.go
+++ moetemplate.go
@@ -3,6 +3,7 @@ package moetemplate
 import (
 	"log"
 	"regexp"
+	"strconv"
 	"strings"
 )
 
@@ -23,27 +24,27 @@ func replaceAll(input string, htmlparts map[string]string, contents map[string]s
 }
 func replace_LoadtemplatePart(input string, key string, value string) string {
 	var regexstr string = `{{LoadTemplate(.{0,20})}}`
-	return replace_regex(input, 15, len(regexstr)-15-3, input, key, value)
+	return replace_regex(regexstr, 15, len(regexstr)-4-14, input, key, value)
 }
 
 func Replace_keyvalue(input string, key string, value string) string {
 	var regexstr string = `{{=.{0,20}}}`
-	return replace_regex(regexstr, 3, len(regexstr)-2-3, input, key, value)
+	return replace_regex(regexstr, 3, len(regexstr)-4-1, input, key, value)
 }
 func replace_regex(regexstr string, namestart int, namelength int, input string, key string, value string) string {
 	reg := regexp.MustCompile(regexstr)
-	var mc []string = reg.FindAllString(input, -1)
+	var mc []string
 	mc = reg.FindAllString(input, -1)
 	var result string = input
 	for i := 0; i < len(mc); i++ {
 		var tag string = mc[i]
 		rs := []rune(tag)
-		log.Println(tag)
+		log.Println(tag + "\t" + strconv.Itoa(namestart) + "\t" + strconv.Itoa(namelength))
 		var str = (rs[namestart:namelength])
 		log.Println(string(str))
 		var pagename = strings.TrimSpace(string(str))
 		if key == pagename {
-			result = strings.Replace(pagename, key, value, -1)
+			result = strings.Replace(result, key, value, -1)
 		}
 	}
 	return result
