a84555f1bb9c04a1277391b23cb8804ce3e4de30
timeloveboy <734991033@qq.com>
2016-01-14 19:59:47 +0800

    s

--- moetemplate.go
+++ moetemplate.go
@@ -23,27 +23,27 @@ func replaceAll(input string, htmlparts map[string]string, contents map[string]s
 }
 func replace_LoadtemplatePart(input string, key string, value string) string {
 	var regexstr string = `<!--{{LoadTemplate(.{0,20})}}-->`
-	return replace_regex(regexstr, 4+15, 15+3+3, input, key, value)
+	return replace_regex(regexstr, 19, 6, input, key, value)
 }
 
 func Replace_keyvalue(input string, key string, value string) string {
 	var regexstr string = `<!--{{=.{0,20}}}-->`
-	return replace_regex(regexstr, 4+3, 3+2+3, input, key, value)
+	return replace_regex(regexstr, 7, 5, input, key, value)
 }
 
 func Replace_Repeat(input string, key string, value string) string {
 	var regexstr string = `<!--{{Repeat(.{0,20})}}-->`
-	return replace_regex(regexstr, 4+9, 9+3+3, input, key, value)
+	return replace_regex(regexstr, 13, 6, input, key, value)
 }
 
-func replace_regex(regexstr string, namestart int, otherlength int, input string, key string, value string) string {
+func replace_regex(regexstr string, namestart int, endlength int, input string, key string, value string) string {
 	reg := regexp.MustCompile(regexstr)
 	var mc []string
 	mc = reg.FindAllString(input, -1)
 	var result string = input
 	for i := 0; i < len(mc); i++ {
 		var tag string = mc[i]
-		var str = xstring.Substring(tag, namestart, len(tag)-otherlength)
+		var str = tag[namestart : len(tag)-endlength]
 		var pagename = xstring.Trim(str)
 		if key == pagename {
 			result = xstring.Replace(result, tag, value)
